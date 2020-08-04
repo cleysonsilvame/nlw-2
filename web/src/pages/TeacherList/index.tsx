29b825f1971adaae1b5920e57cda2486121115f8
Cleyson Silva <62582404+cleysonsilvame@users.noreply.github.com>
2020-08-04 17:02:57 -0300

    Create Teacher Item component

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
-
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+import TeacherItem from '../../components/TeacherItem';
 
 import './style.css';
 
@@ -33,30 +32,7 @@ function TeacherList() {
       </PageHeader>
 
       <main>
-        <article className="teacher-item">
-          <header>
-            <img src="https://avatars1.githubusercontent.com/u/62582404?s=460&u=29db25e5cdf416891054e2130bb29b59ec824898&v=4" alt="Cleyson Silva"/>
-            <div>
-              <strong>Cleyson Silva</strong>
-              <span>Química</span>
-            </div>
-          </header>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            <br/><br/>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt dolores odio ratione unde laborum corporis, dolor velit accusamus adipisci distinctio ex, doloribus, dicta beatae impedit soluta illum tempora vel debitis.
-          </p>
-          <footer>
-            <p>
-              Preço/Hora
-              <strong>R$ 80,00</strong>
-            </p>
-            <button type="button">
-              <img src={whatsappIcon} alt="Entrar em contato"/>
-              Entre em contato
-            </button>
-          </footer>
-        </article>
+        <TeacherItem/>
       </main>
     </div>
   );
