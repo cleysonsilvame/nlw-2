e332bb7bfa256c201e0186378d82e7e796f7928c
Cleyson Silva <62582404+cleysonsilvame@users.noreply.github.com>
2020-08-04 16:18:34 -0300

    Teacher Item for Mobile

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+
 import './style.css';
 
 function TeacherList() {
@@ -29,6 +31,33 @@ function TeacherList() {
           </div>
         </form>
       </PageHeader>
+
+      <main>
+        <article className="teacher-item">
+          <header>
+            <img src="https://avatars1.githubusercontent.com/u/62582404?s=460&u=29db25e5cdf416891054e2130bb29b59ec824898&v=4" alt="Cleyson Silva"/>
+            <div>
+              <strong>Cleyson Silva</strong>
+              <span>Química</span>
+            </div>
+          </header>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            <br/><br/>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt dolores odio ratione unde laborum corporis, dolor velit accusamus adipisci distinctio ex, doloribus, dicta beatae impedit soluta illum tempora vel debitis.
+          </p>
+          <footer>
+            <p>
+              Preço/Hora
+              <strong>R$ 80,00</strong>
+            </p>
+            <button type="button">
+              <img src={whatsappIcon} alt="Entrar em contato"/>
+              Entre em contato
+            </button>
+          </footer>
+        </article>
+      </main>
     </div>
   );
 }
