9d0e47b1a1b01308cc67e9b08fa331e513a8515c
Cleyson Silva <62582404+cleysonsilvame@users.noreply.github.com>
2020-08-06 15:36:09 -0300

    Create Connections Controller

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -11,7 +11,11 @@ interface ScheduleItem{
 
 export default class ClassesController{
   async index (request: Request, response: Response){
-    const filters = request.body;
+    const filters = request.query;
+
+    const week_day = filters.week_day as string;
+    const subject = filters.subject as string;
+    const time = filters.time as string;
 
     if (!filters.week_day || !filters.subject || !filters.time){
       return response.status(400).json({
@@ -19,9 +23,23 @@ export default class ClassesController{
       })
     }
 
-    const timeInMinutes = convertHourToMinutes(filters.time as string);
-    console.log(timeInMinutes);
-    response.send();
+    const timeInMinutes = convertHourToMinutes(time);
+
+    const classes = await db('classes')
+      .whereExists(function(){
+        this.select('class_schedule.*')
+          .from('class_schedule')
+          .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+          .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+          .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
+          .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])
+      })
+      .where('classes.subject', '=', subject)
+      .join('users', 'classes.user_id', '=', 'users.id')
+      .select(['classes.*', 'users.*']);
+
+
+    response.json(classes);
 
   }
 
