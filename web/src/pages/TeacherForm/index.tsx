5b1ef64070fa873e3ba22285e29f65430b32c521
Cleyson Silva <62582404+cleysonsilvame@users.noreply.github.com>
2020-08-10 19:12:13 -0300

    Select component in Teacher Form

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
+import Select from '../../components/Select';
+import Textarea from '../../components/Textarea';
 
 import warningIcon from '../../assets/images/icons/warning.svg'
 
 import './style.css';
-import Textarea from '../../components/Textarea';
 
 function TeacherForm() {
   return (
@@ -28,7 +29,21 @@ function TeacherForm() {
         <fieldset>
           <legend>Sobre a Aula</legend>
 
-          <Input name="subject" label="Matéria"/>
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: 'Artes', label: 'Artes'},
+              { value: 'Biologia', label: 'Biologia'},
+              { value: 'Ciências', label: 'Ciências'},
+              { value: 'Educação Física', label: 'Educação Física'},
+              { value: 'Geografia', label: 'Geografia'},
+              { value: 'História', label: 'História'},
+              { value: 'Matemática', label: 'Matemática'},
+              { value: 'Português', label: 'Português'},
+              { value: 'Quimíca', label: 'Quimíca'},
+            ]}
+          />
           <Input name="cost" label="Custo da sua hora por aula"/>
         </fieldset>
 
